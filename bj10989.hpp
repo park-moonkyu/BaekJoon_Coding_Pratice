2a279d235c24d756849a577afe87289dd0d1e826
park-moonkyu <mk_crew@naver.com>
2020-05-15 22:58:33 +0900

    Create bj10989.hpp
    백준 정렬

---
+++ bj10989.hpp
@@ -0,0 +1,14 @@
+//
+//  bj10989.hpp
+//  Helloworld
+//
+//  Created by Mooongs on 2020/05/14.
+//  Copyright © 2020 Mooongs. All rights reserved.
+//
+
+#ifndef bj10989_hpp
+#define bj10989_hpp
+
+#include <stdio.h>
+
+#endif /* bj10989_hpp */
